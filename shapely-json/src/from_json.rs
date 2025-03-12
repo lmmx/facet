7ad4ad1798a84a63de9fddaa899d5194b94ceba5
Amos Wenger <amos@bearcove.net>
2025-03-12 08:40:40 +0100

    WIP: make json parser iterative

--- shapely-json/src/from_json.rs
+++ shapely-json/src/from_json.rs
@@ -10,66 +10,128 @@ pub fn from_json<'input>(
     trace!("Starting JSON deserialization");
     let mut parser = JsonParser::new(json);
 
-    fn deserialize_value<'input>(
-        parser: &mut JsonParser<'input>,
-        partial: &mut Partial,
-    ) -> Result<(), JsonParseErrorWithContext<'input>> {
-        let shape_desc = partial.shape();
-        let shape = shape_desc.get();
-        trace!("Deserializing value with shape:\n{:?}", shape);
+    // Define our state machine states
+    enum DeserializeState<'a> {
+        Value {
+            partial: Partial<'a>,
+        },
+        StructField {
+            parent_partial: Partial<'a>,
+            key: String,
+            partial_field: Partial<'a>,
+        },
+        StructContinue {
+            partial: Partial<'a>,
+            first: bool,
+        },
+    }
 
-        match &shape.innards {
-            Innards::Scalar(scalar) => {
-                let slot = partial.scalar_slot().expect("Scalar slot");
-                trace!("Deserializing \x1b[1;36mscalar\x1b[0m, \x1b[1;35m{scalar:?}\x1b[0m");
+    // Create our stack and initialize with the root value
+    let mut stack = Vec::new();
+    stack.push(DeserializeState::Value {
+        partial: std::mem::replace(partial, Partial::alloc(partial.shape())),
+    });
 
-                match scalar {
-                    Scalar::String => slot.fill(parser.parse_string()?),
-                    Scalar::U64 => slot.fill(parser.parse_u64()?),
-                    // Add other scalar types as needed
+    while let Some(mut state) = stack.pop() {
+        match state {
+            DeserializeState::Value { mut partial } => {
+                let shape_desc = partial.shape();
+                let shape = shape_desc.get();
+                trace!("Deserializing value with shape:\n{shape:?}");
+
+                match &shape.innards {
+                    Innards::Scalar(scalar) => {
+                        let slot = partial.scalar_slot().expect("Scalar slot");
+                        trace!(
+                            "Deserializing \x1b[1;36mscalar\x1b[0m, \x1b[1;35m{scalar:?}\x1b[0m"
+                        );
+
+                        match scalar {
+                            Scalar::String => slot.fill(parser.parse_string()?),
+                            Scalar::U64 => slot.fill(parser.parse_u64()?),
+                            // Add other scalar types as needed
+                            _ => {
+                                warn!("Unsupported scalar type: {scalar:?}");
+                                return Err(parser.make_error(JsonParseErrorKind::Custom(
+                                    format!("Unsupported scalar type: {scalar:?}"),
+                                )));
+                            }
+                        }
+                    }
+                    Innards::Struct { .. } => {
+                        trace!("Deserializing \x1b[1;36mstruct\x1b[0m");
+                        // Push the struct state on the stack to continue after we process the fields
+                        stack.push(DeserializeState::StructContinue {
+                            partial,
+                            first: true,
+                        });
+                    }
+                    // Add support for other shapes (Array, Transparent) as needed
                     _ => {
-                        warn!("Unsupported scalar type: {:?}", scalar);
+                        error!("Unsupported shape: {shape}");
                         return Err(parser.make_error(JsonParseErrorKind::Custom(format!(
-                            "Unsupported scalar type: {:?}",
-                            scalar
+                            "Unsupported shape: {:?}",
+                            shape.innards
                         ))));
                     }
                 }
             }
-            Innards::Struct { .. } => {
-                trace!("Deserializing \x1b[1;36mstruct\x1b[0m");
+            DeserializeState::StructField {
+                parent_partial,
+                key,
+                partial_field,
+            } => {
+                // Set up the next state to continue parsing the struct after this field
+                stack.push(DeserializeState::StructContinue {
+                    partial: parent_partial,
+                    first: false,
+                });
 
-                let mut first = true;
-                while let Some(key) = if first {
-                    first = false;
+                // Push the value we need to deserialize
+                stack.push(DeserializeState::Value {
+                    partial: partial_field,
+                });
+            }
+            DeserializeState::StructContinue { mut partial, first } => {
+                let key = if first {
                     parser.expect_object_start()?
                 } else {
                     parser.parse_object_key()?
-                } {
-                    trace!("Processing struct key: \x1b[1;33m{}\x1b[0m", key);
-                    let slot = partial
-                        .slot_by_name(&key)
-                        .map_err(|_| parser.make_error(JsonParseErrorKind::UnknownField(key)))?;
-                    let mut partial_field = Partial::alloc(slot.shape());
-                    deserialize_value(parser, &mut partial_field)?;
-                    slot.fill_from_partial(partial_field);
-                }
-                trace!("Finished deserializing \x1b[1;36mstruct\x1b[0m");
+                };
 
-                // TODO: this would be a good place to decide what to do about unset fields? Is this
-                // where we finally get to use `set_default`?
-            }
-            // Add support for other shapes (Array, Transparent) as needed
-            _ => {
-                error!("Unsupported shape: {:?}", shape.innards);
-                return Err(parser.make_error(JsonParseErrorKind::Custom(format!(
-                    "Unsupported shape: {:?}",
-                    shape.innards
-                ))));
+                if let Some(key) = key {
+                    trace!("Processing struct key: \x1b[1;33m{key}\x1b[0m");
+                    let slot = match partial.slot_by_name(&key) {
+                        Ok(slot) => slot,
+                        Err(_) => {
+                            return Err(parser.make_error(JsonParseErrorKind::UnknownField(key)))
+                        }
+                    };
+
+                    let partial_field = Partial::alloc(slot.shape());
+
+                    // Push back the current struct state
+                    stack.push(DeserializeState::StructContinue {
+                        partial,
+                        first: false,
+                    });
+
+                    // Push the field state which will deserialize the value
+                    stack.push(DeserializeState::StructField {
+                        parent_partial: partial,
+                        key,
+                        partial_field,
+                    });
+                } else {
+                    // No more fields, we're done with this struct
+                    trace!("Finished deserializing \x1b[1;36mstruct\x1b[0m");
+
+                    // TODO: this would be a good place to decide what to do about unset fields? Is this
+                    // where we finally get to use `set_default`?
+                }
             }
         }
-        Ok(())
     }
 
-    deserialize_value(&mut parser, partial)
+    Ok(())
 }
